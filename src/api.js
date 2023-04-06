84a2779f24c5ad07f067c13a4291c029953569af
NurkaAmre <nurkerey30@gmail.com>
2023-04-06 15:40:21 +0600

    Setup currentDay

--- src/api.js
+++ src/api.js
@@ -1,11 +1,33 @@
 //Base URL
 const base_url = 'https://api.rawg.io/api';
 
-//Getting the date
+//Getting the month
 const getCurrentMonth = () => {
   const month = new Date().getMonth() + 1;
-  console.log(month)
+  if (month < 10) {
+    return `0${month}`;
+  } else {
+    return month
+  }
 }
+//Getting the date
+const getCurrentDay = () => {
+  const day = new Date().getDate();
+  if (day < 10) {
+    return `0${day}`;
+  } else {
+    return day;
+  }
+};
+
+// Current day/month/year
+const currentYear = new Date().getFullYear();
+const currentMonth = getCurrentMonth();
+const currentDay = getCurrentDay();
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+
 //Popular games
 const popular_games = 'https://api.rawg.io/api/games?dates = 2020,0';
 
