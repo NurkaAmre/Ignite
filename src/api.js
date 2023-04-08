0133c54e2ac463554c3b7cc25fedbb467f8fc010
NurkaAmre <nurkerey30@gmail.com>
2023-04-08 16:40:32 +0600

    Fetch upcomign games, new games

--- src/api.js
+++ src/api.js
@@ -32,5 +32,9 @@ const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
 
 //Popular games
 const popular_games = `games?${key_url}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+const upcoming_games = `games?${key_url}&dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
+const new_games = `games?${key_url}&dates=${lastYear},${currentDate}&ordering=-released&page_size=10`;
 
 export const popularGamesURL = () => `${base_url}${popular_games}`
+export const upcomingGamesURL = () => `${base_url}${upcoming_games}`;
+export const newGamesURL = () => `${base_url}${new_games}`;
